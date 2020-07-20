f2cb5f6330198546f79edd76af0b8f9c53c98403
Catherine <wambukukiiru@gmail.com>
2020-07-20 22:35:00 +0300

    added new variables to JS file

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
+let gender1='male'
+let gender2='female'
 
 function login(gender1, gender2){
     if (gender1 ==='male' && gender2 ==='female'){
         alert ('correct answer!')
     }
-else if(gender2==='female'){
-    alert ('Also correct answer!')
+    else  {
+    alert ('Incorrect answer!!')
     }
 }
 login('male, female')
