3de33b99dec8c840b35829e9bac136a89c03ee25
Catherine <wambukukiiru@gmail.com>
2020-07-20 21:10:01 +0300

    added button elements from bootstrap

--- index.js
+++ index.js
@@ -5,10 +5,4 @@ let add1=function(){
 
     console.log('1 am anonoymous!')
 }
-let outputDiv = document.getElementById('output')
-console.log(outputDiv)
-outputDiv.innerHTML='AKAN NAME'
-
-if (Sunday= 'Kwasi', Monday='Kwadwo', Tuesday='Kwabena', Wednesday='Kwaku', Thursday='Yaw', Friday='Kofi', Saturday='Kwame' ) {
-prompt
-}
+let outputDiv = document.getElementById('output').value 
