382f02445345b3854d7cd375471d151abe27e281
Catherine <wambukukiiru@gmail.com>
2020-07-20 20:36:59 +0300

    added the link to javascript file

---
+++ index.js
@@ -0,0 +1,14 @@
+function add(){
+    console.log('Additional function')
+}
+let add1=function(){
+
+    console.log('1 am anonoymous!')
+}
+let outputDiv = document.getElementById('output')
+console.log(outputDiv)
+outputDiv.innerHTML='AKAN NAME'
+
+if (Sunday= 'Kwasi', Monday='Kwadwo', Tuesday='Kwabena', Wednesday='Kwaku', Thursday='Yaw', Friday='Kofi', Saturday='Kwame' ) {
+prompt
+}
