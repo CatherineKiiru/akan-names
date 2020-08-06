33914095b5b574bff0aa59a59d3b111cfaa59f2f
Catherine <wambukukiiru@gmail.com>
2020-08-07 02:28:02 +0300

    changed classes to id under date field

--- index.js
+++ index.js
@@ -1,22 +1,5 @@
+let Male = document.getElementById('Male').value;
+let Female = document.getElementById('Female').value;
 
-let gender1='male'
-let gender2='female'
-
-function submit(){
-    if (gender1 ==='male' && gender2 ==='female'){
-        confirm ('correct answer!')
-    }
-    else if (gender1 !=='male' && gender2 !=='female'){
-        alert ('Incorrect answer!')
-    }
-}
-    submit('male, female')
-
-   let arrayMale = [Sunday='Kwasi', Monday='Kwadwo', Tuesday='Kwabena',
-    Wednesday='Kwaku', Thursday='Yaw', Friday='Kofi',  Saturday='Kwame'] 
-  
-    let arrayFemale = [Sunday='Akosua', Monday='Adwoa', Tuesday='Abenaa',
-    Wednesday='Akua', Thursday='Yaa', Friday='Afua',  Saturday='Ama'] 
-    
         
     
