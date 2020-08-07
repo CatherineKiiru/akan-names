cc4dd7b633cc958c3ed1d6d5505a7a0e8023f4b7
Catherine <wambukukiiru@gmail.com>
2020-08-07 03:59:39 +0300

    added JS formulas to html form

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
-function checkName(){
-    event.preventDefault()
-    let date = document.getElementById('date').value;
-    let day = new Date(date).toDateString().split(' ')[0];
-    let gender= document.getElementById('gender').value;
-    alert(`${day} ${gender}`)
+let femaleNickNames = ['Akosua','Adowa','Abenaa','Akua','Yaa','Afua','Ama']
+let maleNickNames = ['Kwasi','Kwadwo', 'Kwabena','Kwaku','Yaw','Kofi','Kwame']
+let output = document.getElementById('output')
+let form = document.getElementById('form')
+function result(day,gender){
+    if(gender==='male'){
+        alert(femaleNickNames[day])
+    }
+    else 
+        alert(femaleNickNames[day])
 }
 
-    
+    form.reset()
+
+function checkName(){
+    event.preventDefault()
+    let formDate = document.getElementById('date').value;
+    let date = new Date(formDate);
+    let day = date.getDay();
+    let gender = document.getElementById('gender').value;
+    let today = new Date()
+    if(formDate.length>0 && date<=today){
+        result(day,gender)
+
+    }else if(date>today){
+        alert('Please select a your gender and date of birth')
+    }       
+}
