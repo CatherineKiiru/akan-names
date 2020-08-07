9b6e517312becfde51475c8198928f44866bd1d6
Catherine <wambukukiiru@gmail.com>
2020-08-07 03:00:47 +0300

    added JS function

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-let Male = document.getElementById('Male').value;
-let Female = document.getElementById('Female').value;
+function checkName(){
+    event.preventDefault()
+    let date = document.getElementById('date').value;
+    let day = new Date(date).toDateString().split(' ')[0];
+    let gender= document.getElementById('gender').value;
+    alert(`${day} ${gender}`)
+}
 
-        
     
